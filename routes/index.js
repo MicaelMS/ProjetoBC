bef793a8f7f7a5b4e6bfb17c068226569125493d
MicaelMS <micaelmendesdesouza@gmail.com>
2023-09-08 15:30:52 -0300

    Adicionadas bibliotecas

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,18 @@
 var express = require('express');
 var app = express();
 
-app.get('/', function (req, res){
-    res.send('Olá Mundo!!!');
+// Configurar EJS como mecanismo de visualização
+app.set('view engine', 'ejs');
+app.set('views', './views');
+
+// Rota para renderizar o arquivo index.ejs
+app.get('/', function (req, res) {
+    res.render('index');
 });
+
+app.use(express.static('public'));
 
 app.listen(3000, function () {
     console.log('Executado');
+    console.log("http://localhost:3000");
 });
