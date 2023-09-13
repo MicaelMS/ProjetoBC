2091af41e949d87ea0118ec05e3781bb2cb07a3c
MicaelMS <micaelmendesdesouza@gmail.com>
2023-09-12 23:48:35 -0300

    Home alterada para receber os artigos de forma generica

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,42 @@
 var express = require('express');
 var app = express();
+const ejs = require('ejs');
+const path = require('path');
+const fs = require('fs');
 
 // Configurar EJS como mecanismo de visualização
 app.set('view engine', 'ejs');
 app.set('views', './views');
 
+// Define o caminho completo para o arquivo dados.json
+const articlesFilePath = path.join(__dirname, '..', 'data', 'articles.json');
+const usersFilePath = path.join(__dirname, '..', 'data', 'users.json');
+
+// Leitura do arquivo JSON de dados
+const articlesJSON = fs.readFileSync(articlesFilePath, 'utf8');
+const articles = JSON.parse(articlesJSON);
+const usersJSON = fs.readFileSync(usersFilePath, 'utf8');
+const users = JSON.parse(usersJSON);
+
 // Rota para renderizar o arquivo index.ejs
 app.get('/', function (req, res) {
-    res.render('index');
+    res.render('index', {articles});
+});
+
+app.get('/login', (req, res) => {
+    res.render("login");
+  });
+
+app.get('/adm', (req, res) => {
+    res.render("adm");
+});
+
+app.get('/cadastra_usuario', (req, res) => {
+    res.render("users_create");
+  });
+  
+app.get('/cadastra_artigo', (req, res) => {
+    res.render("articles_create");
 });
 
 app.use(express.static('public'));
